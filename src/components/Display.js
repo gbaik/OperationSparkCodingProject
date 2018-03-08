aeb7b59f6eb5bd15d31d26ea3f85f0ba989a2ed3
Gideon Baik <gideonbaik@gmail.com>
2018-03-07 17:48:32 -0800

    Remove socket.io

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,18 +2,14 @@ import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import List from './List.js';
 import { exampleData } from '../data/example';
-import io from 'socket.io-client';
+import List from './List.js';
 import Pagination from './Pagination.js';
 
 class Display extends Component { 
   constructor(props) { 
     super(props);
-    
-    // this.port = process.env.port || 5000;
-    // this.socket = io(`http://localhost:${this.port}`);
-
+  
     this.state = {
       pageNumbers: [],
       currentPage: 1,
@@ -22,31 +18,21 @@ class Display extends Component {
     }
 
     this.handlePageClick = this.handlePageClick.bind(this);
-    // console.log(this.props);
   }
 
   componentDidMount() {
-    // this.socket.on('get_cryptocurrency_data', (lastUpdated) => {
-    //   Meteor.call('getData', (error, response) => {
-    //     if (error) {
-    //       throw err;
-    //     }
+    // console.log(this.props);
+    // const data = response.data;
+    // let temp = [];
+    
+    // for (var i = 1; i <= Math.ceil(data.length / 10); i++) {
+    //   temp.push(i);
+    // }
 
-    //     const data = response.data;
-    //     let temp = [];
-        
-    //     for (var i = 1; i <= Math.ceil(data.length / 10); i++) {
-    //       temp.push(i);
-    //     }
-
-    //     this.setState({
-    //       pageNumbers: temp,         
-    //       cryptocurrencyData: data,
-    //       lastUpdated: lastUpdated          
-    //     });
-
-    //     Meteor.call('cryptocurrencyData.insert', data);      
-    //   });
+    // this.setState({
+    //   pageNumbers: temp,         
+    //   cryptocurrencyData: data,
+    //   lastUpdated: lastUpdated          
     // });
   }
 
@@ -57,8 +43,6 @@ class Display extends Component {
   }
 
   render() {
-    console.log(this.props);
-
     const { cryptocurrencyData, currentPage, pageNumbers, lastUpdated } = this.state;
     
     const indexOfLastPage = currentPage * 10;
